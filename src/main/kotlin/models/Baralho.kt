54bc86f9d970bbbeaafdc944dc20206d819aabde
Guilherme <guifranca60@gmail.com>
2025-04-19 01:35:20 -0300

    Espelhar entidades com as tabelas do banco

--- src/main/kotlin/models/Baralho.kt
+++ src/main/kotlin/models/Baralho.kt
@@ -1,25 +1,28 @@
-package com.example.flashlearn.data.models
+import org.bson.codecs.pojo.annotations.BsonId
+import kotlinx.serialization.Serializable
+import org.bson.types.ObjectId
+import java.time.LocalDateTime
 
-import androidx.room.Entity
-import androidx.room.PrimaryKey
-import androidx.room.ForeignKey
-
-@Entity(
-    tableName = "baralhos",
-    foreignKeys = [
-        ForeignKey(
-            entity = Usuario::class,
-            parentColumns = ["id"],
-            childColumns = ["usuarioId"],
-            onDelete = ForeignKey.CASCADE
-        )
-    ]
-)
+@Serializable
 data class Baralho(
-    @PrimaryKey val id: String,
-    val nome: String,
-    val descricao: String,
-    val usuarioId: String,
-    val dataCriacao: Long = System.currentTimeMillis(),
-    val ultimaModificacao: Long = System.currentTimeMillis()
-) 
+    @BsonId val id: ObjectId = ObjectId(),
+    val cartas: List<Carta>,
+    val id_usuario: String
+)
+
+@Serializable
+data class Carta(
+    val topico: String,
+    val tipo: String, // Ex: "multipleChoice"
+    val pergunta: String,
+    val resposta: Int, // índice da alternativa correta
+    val alternativas: List<String>,
+    val localizacao: Localizacao,
+    val proxima_revisao: String
+)
+
+@Serializable
+data class Localizacao(
+    val lat: Double,
+    val lon: Double
+)
--- src/main/kotlin/models/Usuario.kt
+++ src/main/kotlin/models/Usuario.kt
@@ -1,13 +1,13 @@
-package com.example.flashlearn.data.models
-
-import androidx.room.Entity
-import androidx.room.PrimaryKey
-
-@Entity(tableName = "usuarios")
-data class Usuario(
-    @PrimaryKey val id: String,
-    val nome: String,
-    val email: String,
-    val localizacao: String? = null,
-    val dataCriacao: Long = System.currentTimeMillis()
-) 
+//package com.example.flashlearn.data.models
+//
+//import androidx.room.Entity
+//import androidx.room.PrimaryKey
+//
+//@Entity(tableName = "usuarios")
+//data class Usuario(
+//    @PrimaryKey val id: String,
+//    val nome: String,
+//    val email: String,
+//    val localizacao: String? = null,
+//    val dataCriacao: Long = System.currentTimeMillis()
+//)
