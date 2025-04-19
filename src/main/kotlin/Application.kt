37f87b30e0c9ac9eb13b380e1efb94820d7e2556
Guilherme <guifranca60@gmail.com>
2025-04-19 01:37:13 -0300

    implementacao da persistencia e geracao de end-points

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -1,12 +1,35 @@
 package com.ppm
 
+import Baralho
+import com.ppm.database.MongoClientProvider
+import com.ppm.repository.BaralhoRepository
+import com.ppm.routes.baralhoRoutes
 import io.ktor.server.application.*
+import io.ktor.server.routing.routing
+import kotlinx.coroutines.runBlocking
+import org.litote.kmongo.coroutine.coroutine
+import org.litote.kmongo.reactivestreams.KMongo
 
 fun main(args: Array<String>) {
     io.ktor.server.netty.EngineMain.main(args)
 }
 
 fun Application.module() {
+    val client = KMongo.createClient().coroutine
+    val db = client.getDatabase("Flashcard")
+    val flashcardRepo = BaralhoRepository(db)
+
+    runBlocking {
+        val resultados: List<Baralho> = flashcardRepo.buscarTodos()
+        println("Baralhos encontrados na inicialização:")
+        resultados.forEach { println(it) }
+    }
+
+    routing {
+        baralhoRoutes()
+    }
+
+
     configureSerialization()
     configureHTTP()
     configureRouting()
