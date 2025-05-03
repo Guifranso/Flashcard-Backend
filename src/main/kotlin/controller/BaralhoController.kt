04c85ab7a3062f5efab8444e382af3b8196b0fc1
Pedro Biagi <pedro.biagi@ufu.br>
2025-05-02 22:53:11 -0300

    Tentativa Melhoria CRUD do baralho

--- src/main/kotlin/controller/BaralhoController.kt
+++ src/main/kotlin/controller/BaralhoController.kt
@@ -4,6 +4,7 @@ import Baralho
 import com.ppm.repository.BaralhoRepository
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.asFlow
+import org.bson.types.ObjectId
 
 class BaralhoController(
     private val repository: BaralhoRepository
@@ -12,4 +13,22 @@ class BaralhoController(
         val lista = repository.buscarTodos()
         return lista.asFlow()
     }
-} 
+
+    /*
+    suspend fun getBaralhoById(id: ObjectId): Baralho? {
+        return repository.buscarPorId(id)
+    }
+
+    suspend fun inserirBaralho(baralho: Baralho) {
+        repository.inserir(baralho)
+    }
+
+    suspend fun atualizarBaralho(baralho: Baralho) {
+        repository.atualizar(baralho)
+    }
+
+    suspend fun deletarBaralho(id: ObjectId) {
+        repository.deletar(id)
+    }
+    */
+}
